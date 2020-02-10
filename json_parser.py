6e206421c3dbecb53e9ab50178a60e621f10d41b
Govender, Justin <jmg66@students.waikato.ac.nz>
2020-02-10 15:35:30 +0900

    Now recursively looks through json file for matching keys.

--- json_parser.py
+++ json_parser.py
@@ -5,23 +5,36 @@ import json
 
 
 def json_parse(file):
-    out = ''
-    json_keys = ['title', 'text']
+    key_list = ['title', 'text']
     with open(file, 'r') as f:
         json_list = json.load(f)
-        # For each dictionary in the list...
-        for i, d in enumerate(json_list):
-            # Get the data dictionary
-            data = d.get('data')
-            if data is not None:
-                for j, val in enumerate(data):
-                    if val.get('title') is not None:
-                        out += val.get('title') + '\n'
-                    if val.get('text') is not None:
-                        print(val.get('text'))
-                        out += val.get('text') + '\n'
+        return recursive_list_unpacker(json_list, key_list)
 
-        return out
+
+def recursive_dict_unpacker(json_dict, key_list):
+    output = ''
+    # Go through the dictionary looking for lists
+    for value in json_dict.values():
+        if isinstance(value, list):
+            # If the value is a list then call the list fn
+            output += recursive_list_unpacker(value, key_list)
+
+    # Find matching key pair values
+    for i in json_dict:
+        if i in key_list:
+            output += json_dict.get(i) + '\n'
+
+    return output
+
+
+def recursive_list_unpacker(json_list, key_list):
+    output = ''
+    # Go through the list looking for dictionaries
+    for i in json_list:
+        if isinstance(i, dict):
+            # Call recursive dict fn
+            output += recursive_dict_unpacker(i, key_list)
+    return output
 
 
 def parse_args(argv):
@@ -30,7 +43,7 @@ def parse_args(argv):
     parser = argparse.ArgumentParser(description='json_parser_args')
     # set the argument formats
     parser.add_argument(
-        '--file', default=os.path.join('.', 'kubernetes.json'),
+        '--file', default=os.path.join('.', 'json_files', 'kubernetes.json'),
         help='json file to be parsed')
 
     return parser.parse_args(argv[1:])
